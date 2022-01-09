c07c927fc59502846a68e05800732121a98b9eb4
popeyewrener <72228782+popeyewrener@users.noreply.github.com>
2022-01-09 18:04:01 +0530

    solution of all question

---
+++ solution.py
@@ -0,0 +1,52 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\Ranjeeta\Music\py_assign\all_question.py ==========
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 1
+Enter 1st number: 123
+Enter 2nd number: 322
+Enter 3rd number: 43121
+The average of 123.0 322.0 43121.0 is 14522.0
+Do you want to try something else or quit ('Y' to try 'N' to quit)y
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 2
+Enter your gross income in dollars: 213123132
+Enter total dependents: 12
+Your income tax is:  42615426.4
+Do you want to try something else or quit ('Y' to try 'N' to quit)y
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 3
+Enter SID: 1234
+Enter name: popeye
+Enter Gender(M,F,U for unknown)m
+Enter course name: CSE UG
+Enter CGPA9
+['1234', 'popeye', 'm', 'CSE UG', 9.0]
+Do you want to try something else or quit ('Y' to try 'N' to quit)y
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 
+Invalid input 
+Taking you to choice again!!
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 4
+Enter mark of Student:21
+Enter mark of Student:32
+Enter mark of Student:12
+Enter mark of Student:111
+Enter mark of Student:41
+[12.0, 21.0, 32.0, 41.0, 111.0]
+Do you want to try something else or quit ('Y' to try 'N' to quit)y
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 5
+Invalid input 5
+Taking you to choice again!!
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 5a
+['red', 'Green', 'White', 'Pink', 'Yellow']
+Do you want to try something else or quit ('Y' to try 'N' to quit)y
+Enter Question number you want to explore: (Valid inputs are '1,2,3,4,5a,5b')
+Enter your choice here: 5b
+['red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
+Do you want to try something else or quit ('Y' to try 'N' to quit)n
+CLOSED PROGRAM
